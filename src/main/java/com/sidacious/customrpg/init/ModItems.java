ced6f76d6c51d2c02e81395a57abc5c099d01f28
sidacious <kevinking188@hotmail.com>
2014-12-28 13:27:29 +0800

    ...

--- src/main/java/com/sidacious/customrpg/init/ModItems.java
+++ src/main/java/com/sidacious/customrpg/init/ModItems.java
@@ -1,6 +1,7 @@
 package com.sidacious.customrpg.init;
 
 
+import com.sidacious.customrpg.item.ItemBasicFoci;
 import com.sidacious.customrpg.item.ItemManaSword;
 import com.sidacious.customrpg.item.ItemCustomRPG;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -8,11 +9,13 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class ModItems
 {
     public static final ItemCustomRPG ItemManaSword = new ItemManaSword();
+    public static final ItemCustomRPG ItemBasicFoci = new ItemBasicFoci();
 
 
     public static void init ()
     {
         GameRegistry.registerItem(ItemManaSword, "manasword");
+        GameRegistry.registerItem(ItemBasicFoci, "basicfoci");
 
 
     }
--- src/main/java/com/sidacious/customrpg/reference/Name.java
+++ src/main/java/com/sidacious/customrpg/reference/Name.java
@@ -5,6 +5,7 @@ public class Name
     public class Items
     {
         public static final String MANA_SWORD = "manasword";
+        public static final String BASIC_FOCI = "basicfoci";
     }
 
 
