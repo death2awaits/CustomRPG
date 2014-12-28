40f3c1015f0c378881af9aa5f69d64111827c604
sidacious <kevinking188@hotmail.com>
2014-12-28 13:29:07 +0800

    ...

---
+++ src/main/java/com/sidacious/customrpg/item/ItemBasicFoci.java
@@ -0,0 +1,13 @@
+package com.sidacious.customrpg.item;
+
+
+import com.sidacious.customrpg.reference.Name;
+
+public class ItemBasicFoci extends ItemCustomRPG
+{
+    public ItemBasicFoci()
+    {
+        super();
+        this.setUnlocalizedName(Name.Items.BASIC_FOCI);
+    }
+}
